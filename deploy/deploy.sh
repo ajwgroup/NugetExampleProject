56dee8ad2c8b31c66318f6b79a5ac3f7c69c2bd9
Adam Worley <adam.worley@ajw-group.com>
2018-12-24 11:24:47 +0000

    Update variable reference

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,6 @@ ApiKey=$1
 ts=$(date +"%y%m%d%H%M")
 
 echo "Starting pack"
-dotnet pack $PROJECT_NAME/$PROJECT_NAME.csproj /p:PackageVersion=1.0.$ts --configuration Release
+dotnet pack $PROJECT_NAME$/$PROJECT_NAME$.csproj /p:PackageVersion=1.0.$ts --configuration Release
 echo "Starting push"
-dotnet nuget push $PROJECT_NAME/bin/Release/*1.0.$ts.nupkg --source https://api.nuget.org/v3/index.json --api-key $ApiKey
+dotnet nuget push $PROJECT_NAME$/bin/Release/*1.0.$ts.nupkg --source https://api.nuget.org/v3/index.json --api-key $ApiKey
